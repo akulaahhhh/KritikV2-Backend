a716cdc1c10d783708cbb6a4ce9459dd02c3878c
asjadZ <asjad@zohlgroup.com>
2025-02-19 16:28:14 +0800

    arghh

--- helpers/driver.py
+++ helpers/driver.py
@@ -2,14 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 chrome_options = Options()
-chrome_options.add_argument("--headless")  # Run Chrome without UI
+chrome_options.add_argument("--headless")  # Run in headless mode (no GUI)
 
-# Use Browserless WebDriver Endpoint (replace YOUR_TOKEN)
+# Use Browserless WebDriver Endpoint
 browserless_url = "https://browserless-production-6684.up.railway.app/webdriver?token=0kWaAdmvtQm0zbBL0PHLFIHnKYRJPlJDe07rKOMjprevNqFp"
 
 def chrome_driver():
     driver = webdriver.Remote(
-        command_executor=browserless_url,
+        command_executor=browserless_url,  # Connect to Browserless
         options=chrome_options
     )
     return driver
