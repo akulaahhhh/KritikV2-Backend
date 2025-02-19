ec396b1822d0699ad49fc38819a2ac45a3caef0c
asjadZ <asjad@zohlgroup.com>
2025-02-19 17:13:36 +0800

    .

--- helpers/driver.py
+++ helpers/driver.py
@@ -1,22 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")  # Run in headless mode
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
 
-# Set the token in capabilities
-caps = DesiredCapabilities.CHROME.copy()
-caps["browserless:token"] = "0kWaAdmvtQm0zbBL0PHLFIHnKYRJPlJDe07rKOMjprevNqFp"
+# Set the token as a capability
+chrome_options.set_capability("browserless:token", "0kWaAdmvtQm0zbBL0PHLFIHnKYRJPlJDe07rKOMjprevNqFp")
 
 browserless_url = "https://browserless-production-6684.up.railway.app/webdriver"
 
 def chrome_driver():
     driver = webdriver.Remote(
         command_executor=browserless_url,  # No token in URL
-        desired_capabilities=caps,
         options=chrome_options
     )
     return driver
